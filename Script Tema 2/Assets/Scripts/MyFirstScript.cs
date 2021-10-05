501b9a605cce4879ce011b6210f8d62208f841a4
Flora de la Cruz <91455093+Gingasi@users.noreply.github.com>
2021-10-05 12:57:06 +0200

    05/10/2021

---
+++ Script Tema 2/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    public int playerAge = 35;
+    private float playesSpeed = 5.25f;
+    public string playerName = "Gingasi";
+    [SerializeField] private bool gameOver = false;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

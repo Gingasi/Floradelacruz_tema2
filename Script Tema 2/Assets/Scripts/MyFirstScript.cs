a9bfbef6501f1b010855f7460bfac3950c189859
Flora de la Cruz <91455093+Gingasi@users.noreply.github.com>
2021-10-18 11:15:19 +0200

    Codigo

--- Script Tema 2/Assets/Scripts/MyFirstScript.cs
+++ Script Tema 2/Assets/Scripts/MyFirstScript.cs
@@ -4,19 +4,52 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+    
     public int playerAge = 35;
     private float playesSpeed = 5.25f;
     public string playerName = "Gingasi";
     [SerializeField] private bool gameOver = false;
 
+    /*
+    public int x = 5;
+    public int y = 2;
 
+    public float a = 5f;
+    public float b = 2f;
 
-
-
+    public bool isRaining = false;
+    */
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*  Debug.Log($"suma: {x} + {y} = {x + y}");
+          Debug.Log("Resta: " + x + " - " + y + " = " + (x - y));
+          Debug.Log($"division: {x} / {y} = {x / y}");
+          Debug.Log($"multiplicacion: {x} * {y} = {x * y}");
+          Debug.Log(message: string.Format("Productor: {0} / {1} = {2}", x, y, x / y));
+
+
+          if (isRaining == true)
+          {
+              Debug.Log(message: "lleva paraguas");
+          } else 
+          {
+              Debug.Log(message: "no llueve. No lleves paraguas");
+          }
+         */
+
+        if (playerAge >= 18)
+        {
+            Debug.Log(message: "Eres un adulto y gilipollas");
+        }
+        else if (playerAge >=13)
+        {
+            Debug.Log(message: "No crezcas, es una trampa");
+        }
+        else 
+        {
+            Debug.Log(message: "No eres nada");
+        }
     }
 
     // Update is called once per frame
--- Script Tema 2/Assets/Scripts/vehicle.cs
+++ Script Tema 2/Assets/Scripts/vehicle.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class vehicle : MonoBehaviour
 {
+    // variable que guarda el sonido del vehiculo
+    // Estas dos variables indicaran el nombre del vehículo y cuantas ruedas tienen ada vehículo 
+
+    [SerializeField] private string sound = "¡broom, broom!";
+    [SerializeField] private string name;
+
+    [SerializeField] int numberwheels = 4;
+
+    public bool hasSiren = true;
+    [SerializeField] private bool isOn = false;
     // Start is called before the first frame update
+
     void Start()
     {
-        
+      //  Debug.Log($"{name} tiene {numberwheels} y hace {sound}");
+
+        if (isOn == true)
+        {
+            Debug.Log(message: $"{name} hace {sound}");
+        } else if (hasSiren == true) {
+            Debug.Log(message: $"{name} hara {sound} cuando se ponga en marcha");
+        }
+        else
+        {
+            Debug.Log(message: $"{name} no está en marcha");
+        }
+
+       
+       
+      
     }
 
     // Update is called once per frame

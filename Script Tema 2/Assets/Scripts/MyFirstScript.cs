33ca444522e90459c57c49253786a2014a5ebb06
Flora de la Cruz <91455093+Gingasi@users.noreply.github.com>
2021-10-28 09:55:02 +0200

    25/10

--- Script Tema 2/Assets/Scripts/MyFirstScript.cs
+++ Script Tema 2/Assets/Scripts/MyFirstScript.cs
@@ -4,57 +4,90 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
-    
-    public int playerAge = 35;
-    private float playesSpeed = 5.25f;
-    public string playerName = "Gingasi";
-    [SerializeField] private bool gameOver = false;
+    public Vector3 Mypos;
 
-    /*
-    public int x = 5;
-    public int y = 2;
+    public string hello; 
 
-    public float a = 5f;
-    public float b = 2f;
-
-    public bool isRaining = false;
-    */
     // Start is called before the first frame update
     void Start()
     {
-        /*  Debug.Log($"suma: {x} + {y} = {x + y}");
-          Debug.Log("Resta: " + x + " - " + y + " = " + (x - y));
-          Debug.Log($"division: {x} / {y} = {x / y}");
-          Debug.Log($"multiplicacion: {x} * {y} = {x * y}");
-          Debug.Log(message: string.Format("Productor: {0} / {1} = {2}", x, y, x / y));
-
-
-          if (isRaining == true)
-          {
-              Debug.Log(message: "lleva paraguas");
-          } else 
-          {
-              Debug.Log(message: "no llueve. No lleves paraguas");
-          }
-         */
-
-        if (playerAge >= 18)
-        {
-            Debug.Log(message: "Eres un adulto y gilipollas");
-        }
-        else if (playerAge >=13)
-        {
-            Debug.Log(message: "No crezcas, es una trampa");
-        }
-        else 
-        {
-            Debug.Log(message: "No eres nada");
-        }
+        HelloWorld();
+        hello = GetHello();
+        Debug.Log(hello);
+        /*
+        Mypos = new Vector3(x: 2, y: 1, z: 0);
+        Mypos = Vector3.zero;
+        Mypos = Vector3.one;
+        Mypos = Vector3.up;
+        Mypos = Vector3.down;
+        Mypos = Vector3.right;
+        Mypos = Vector3.left;
+        Mypos = Vector3.forward;
+        Mypos = Vector3.back;
+        */
+        transform.position = Mypos;
+        
+        
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+     if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += Vector3.right; 
+        }
+     if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left;
+        }
+      if (Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.position += Vector3.up;
+        }
+      if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.position += Vector3.down;
+        }
+       if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += Vector3.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += Vector3.back;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.rotation *= Quaternion.Euler(0, 10, 0); 
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.rotation *= Quaternion.Euler(0, -10, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.rotation *= Quaternion.Euler(10, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.rotation *= Quaternion.Euler(-10, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.localScale += Vector3.one;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            transform.localScale += -1 *Vector3.one;
+        }
+
     }
+    public void HelloWorld()
+    { 
+       Debug.Log(message:"¡Hola Mundo!");
+    }
+    public string GetHello ()
+    { return "Hola"; }
+
 }
--- Script Tema 2/Assets/Scripts/vehicle.cs
+++ Script Tema 2/Assets/Scripts/vehicle.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class vehicle : MonoBehaviour
 {
-    // variable que guarda el sonido del vehiculo
-    // Estas dos variables indicaran el nombre del vehículo y cuantas ruedas tienen ada vehículo 
+    [SerializeField] private bool isOn = true;
+    /* variable que guarda el sonido del vehiculo
+    Estas dos variables indicaran el nombre del vehículo y cuantas ruedas tienen ada vehículo 
 
     [SerializeField] private string sound = "¡broom, broom!";
     [SerializeField] private string name;
@@ -13,32 +14,92 @@ public class vehicle : MonoBehaviour
     [SerializeField] int numberwheels = 4;
 
     public bool hasSiren = true;
-    [SerializeField] private bool isOn = false;
+    
     // Start is called before the first frame update
 
+    [SerializeField] private float gasoline;
+    */
     void Start()
     {
-      //  Debug.Log($"{name} tiene {numberwheels} y hace {sound}");
+        /*
 
-        if (isOn == true)
-        {
-            Debug.Log(message: $"{name} hace {sound}");
-        } else if (hasSiren == true) {
-            Debug.Log(message: $"{name} hara {sound} cuando se ponga en marcha");
-        }
-        else
-        {
-            Debug.Log(message: $"{name} no está en marcha");
-        }
-
+            if (isOn == true)
+            {
+                if (gasoline <= 10)
+                {
+                    Debug.Log(message: $"a {name} le queda poca gasolina");
+                }
+                if (hasSiren == true)
+                {
+                    Debug.Log(message: sound);
+                }
+                else
+                {
+                    Debug.Log(message: $"{name} no tiene sirena");
+                }
+            }
+            else
+            {
+                Debug.Log(message: $"{name} no esta en marcha");
+            }
+        */
        
-       
-      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isOn == true)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.position += Vector3.right;
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.position += Vector3.left;
+            }
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                transform.position += Vector3.up;
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                transform.position += Vector3.down;
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.position += Vector3.forward;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.position += Vector3.back;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.rotation *= Quaternion.Euler(0, 10, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.rotation *= Quaternion.Euler(0, -10, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.rotation *= Quaternion.Euler(10, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                transform.rotation *= Quaternion.Euler(-10, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                transform.localScale += Vector3.one;
+            }
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                transform.localScale += -1 * Vector3.one;
+            }
+
+        }
     }
 }
